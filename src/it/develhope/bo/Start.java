38c20a67f0cec91b68aedf1d672e464b376e614d
Alessio Limina <alessio.limina@gmail.com>
2023-01-24 10:26:19 +0100

    Exercise: Boolean Operators 1

--- src/it/develhope/bo/Start.java
+++ src/it/develhope/bo/Start.java
@@ -1,2 +1,22 @@
-package it.develhope.bo;public class Start {
+package it.develhope.bo;
+
+import java.util.Scanner;
+
+public class Start {
+    public static void main(String[] args) {
+
+        System.out.println(2 <= 2 && !true);
+
+        System.out.println(!false && 3 > 2);
+
+        boolean t = false;
+        boolean f = true;
+
+        System.out.println(!(!t || f));
+
+        System.out.println(6 > 6 ^ !(true && true));*/
+
+    }
 }
+
+
