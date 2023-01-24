e6136c9ef42c355e8109f1d94a215d551b3e31ca
Alessio Limina <alessio.limina@gmail.com>
2023-01-24 10:34:46 +0100

    Exercise: Boolean Operators 1

--- src/it/develhope/bo/Start.java
+++ src/it/develhope/bo/Start.java
@@ -4,17 +4,25 @@ import java.util.Scanner;
 
 public class Start {
     public static void main(String[] args) {
+        System.out.println("--------Starting operations -----------");
 
-        System.out.println(2 <= 2 && !true);
+        System.out.println("2 <= 2 && !true: " + (2 <= 2 && !true));
 
-        System.out.println(!false && 3 > 2);
+        System.out.println("-----------------------------------------");
+
+        System.out.println("!false && 3 > 2: " + (!false && 3 > 2));
+
+        System.out.println("-----------------------------------------");
 
         boolean t = false;
         boolean f = true;
+        System.out.println("Considering that t=false and f=true, !(!t || f): " + (!(!t || f)));
 
-        System.out.println(!(!t || f));
+        System.out.println("------------------------------------------");
 
-        System.out.println(6 > 6 ^ !(true && true));*/
+        System.out.println("6 > 6 ^ !(true && true): " + (6 > 6 ^ !(true && true)));
+
+        System.out.println("----------------------------------------");
 
     }
 }
